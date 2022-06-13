1594f0e9314da9bd76d1dfc31b0defd41917278f
luke-buttifant <lukerhys.lb@gmail.com>
2022-06-13 09:08:21 +0100

    fixed interviewer position

--- components/interview/animations.js
+++ components/interview/animations.js
@@ -1,18 +1,18 @@
 import React, { Suspense, useEffect, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 
 import Interviewer from "../../Interviewer"
+import ConferenceRoom from "../../ConferenceRoom"
 
 const deg2rad = degrees => degrees * (Math.PI / 180);
 
 export default function Animations(questions) {
-
   return (
     <>
           <Canvas
-          camera={{ position: [0, 10, 10], fov: 10}}
+          camera={{ position: [0, 5.35, 10], fov: 80, zoom: 18, near: 0.1, far: 200}}
          style={{
             marginInline: "auto",
             backgroundColor: '#F5F5F5',
@@ -23,8 +23,9 @@ export default function Animations(questions) {
          <ambientLight intensity={0.1} />
          <directionalLight intensity={0.4} />
          <Suspense fallback={null}>
+         <ConferenceRoom position={[0, 0, 0]}  />
          <Interviewer position={[0, 0, 0]} />
-         <OrbitControls />
+         
          </Suspense>
       </Canvas>
     </>
