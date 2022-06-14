bf20a23fe071d43298fc9ef92877d33b1991644a
luke-buttifant <lukerhys.lb@gmail.com>
2022-06-14 16:16:46 +0100

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import '../styles/globals.css'
 import Head from "next/head"
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Navbar from "../components/Navbars/IndexNavbar"
+import Modal from '../components/modal/developmentModal';
 
 export default function App({
   Component,
@@ -33,6 +34,7 @@ export default function App({
       <meta name="author" content="Luke Buttifant" />
     </Head>
     
+    <Modal />
     <SessionProvider session={session}>
     <Navbar fixed />
       <Component {...pageProps} />
